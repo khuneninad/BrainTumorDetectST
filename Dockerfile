458b2e3d4c1223c0099992651a0559b2c0b061ac
Ninad Khune <129279241+khuneninad@users.noreply.github.com>
2025-02-03 19:18:36 +0530

    UploadChecking

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90cf8c6209fb3512681e97edc7aaa2bd51a280c740e5a01c9d997fe78cd4d0fd
+size 184
---
+++ stapp.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a641f99eecf3a1074250a1ad84d3f4d46ac01299ccb5e234cd0588d369e579b0
+size 2174
